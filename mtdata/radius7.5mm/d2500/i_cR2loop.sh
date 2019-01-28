6a98fb23f80ee93fe49b4ea75ee422762389d7fe
ysbd1205 <ys.bd.1205@gmail.com>
2019-01-28 13:30:57 +0900

    no updates

--- mtdata/radius7.5mm/d2500/i_cR2loop.sh
+++ mtdata/radius7.5mm/d2500/i_cR2loop.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 echo "Shellscript start"
 
-cFarray=("0.3" "0.5" "0.7" "0.9")
+#cFarray=("0.3" "0.5" "0.7" "0.9")
 cRFarray=("0.3" "0.5" "0.7" "0.9")
 
 #loop
-for i in `seq 0 1` ; do
-    sed -i -e "18c\variable cF equal ${cFarray[i]}" in.i_yM150e6_ps2_cR2_d2500_r7.5
-    for j in `seq 0 3` ; do
+#for i in `seq 0 1` ; do
+    #sed -i -e "18c\variable cF equal ${cFarray[i]}" in.i_yM150e6_ps2_cR2_d2500_r7.5
+    sed -i -e "18c\variable cF equal 0.3" in.i_yM150e6_ps2_cR2_d2500_r7.5
+    for j in `seq 2 3` ; do
         sed -i -e "19c\variable cRF equal ${cRFarray[j]}" in.i_yM150e6_ps2_cR2_d2500_r7.5
             echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-            echo "cF${cFarray[i]}_cRF${cRFarray[j]}"
+            echo "cF0.3_cRF${cRFarray[j]}"
+            #echo "cF${cFarray[i]}_cRF${cRFarray[j]}"
             echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
         mpirun -np 2 lmp_auto < in.i_yM150e6_ps2_cR2_d2500_r7.5
     done
-done
+#done
