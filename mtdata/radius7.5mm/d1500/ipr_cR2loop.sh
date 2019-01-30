133db7f2928bc534994cc33cd8a9382b16192272
ysbd1205 <ys.bd.1205@gmail.com>
2019-01-30 18:33:38 +0900

    no updates

--- mtdata/radius7.5mm/d1500/ipr_cR2loop.sh
+++ mtdata/radius7.5mm/d1500/ipr_cR2loop.sh
@@ -7,11 +7,11 @@ cRFarray=("0.3" "0.5" "0.7" "0.9")
 #loop
 #for i in `seq 2 3` ; do
     #sed -i -e "18c\variable cF equal ${cFarray[i]}" in.ipr_yM150e6_ps2_cR2_d1500_r7.5
-    sed -i -e "18c\variable cF equal 0.3" in.ipr_yM150e6_ps2_cR2_d1500_r7.5
+    sed -i -e "18c\variable cF equal 0.7" in.ipr_yM150e6_ps2_cR2_d1500_r7.5
     for j in `seq 0 3` ; do
         sed -i -e "19c\variable cRF equal ${cRFarray[j]}" in.ipr_yM150e6_ps2_cR2_d1500_r7.5
             echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-            echo "cF0.3_cRF${cRFarray[j]}"
+            echo "cF0.7_cRF${cRFarray[j]}"
             #echo "cF${cFarray[i]}_cRF${cRFarray[j]}"
             echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
         mpirun -np 2 lmp_auto < in.ipr_yM150e6_ps2_cR2_d1500_r7.5
