0a778e7b9ce8dbce83573be64561d86053fcb579
ysbd1205 <ys.bd.1205@gmail.com>
2019-02-04 22:27:47 +0900

    no updates

--- mtdata/radius7.5mm/d2500/cR4loop.sh
+++ mtdata/radius7.5mm/d2500/cR4loop.sh
@@ -5,10 +5,10 @@ cFarray=("0.3" "0.5" "0.7" "0.9")
 cRFarray=("0.3" "0.5" "0.7" "0.9")
 
 #loop
-for i in `seq 0 1` ; do
+for i in `seq 0 3` ; do
     #sed -i -e "18c\variable cF equal 0.5" in.yM150e6_ps2_cR4_d2500_r7.5
     sed -i -e "18c\variable cF equal ${cFarray[i]}" in.yM150e6_ps2_cR4_d2500_r7.5
-    for j in `seq 1 3` ; do
+    for j in `seq 0 3` ; do
         sed -i -e "19c\variable cRF equal ${cRFarray[j]}" in.yM150e6_ps2_cR4_d2500_r7.5
             echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
             #echo "cF0.5_cRF${cRFarray[j]}"
