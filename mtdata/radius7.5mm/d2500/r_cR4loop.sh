586fa089b90dbe12d0eeae789cb709fc943a45c4
ysbd1205 <ys.bd.1205@gmail.com>
2019-02-05 00:26:57 +0900

    no updates

--- mtdata/radius7.5mm/d2500/r_cR4loop.sh
+++ mtdata/radius7.5mm/d2500/r_cR4loop.sh
@@ -5,14 +5,14 @@ cFarray=("0.3" "0.5" "0.7" "0.9")
 cRFarray=("0.3" "0.5" "0.7" "0.9")
 
 #loop
-for i in `seq 2 3` ; do
-    sed -i -e "18c\variable cF equal ${cFarray[i]}" in.r_yM150e6_ps2_cR2_d2500_r7.5
+for i in `seq 0 3` ; do
+    sed -i -e "18c\variable cF equal ${cFarray[i]}" in.r_yM150e6_ps2_cR4_d2500_r7.5
     for j in `seq 0 3` ; do
-        sed -i -e "19c\variable cRF equal ${cRFarray[j]}" in.r_yM150e6_ps2_cR2_d2500_r7.5
+        sed -i -e "19c\variable cRF equal ${cRFarray[j]}" in.r_yM150e6_ps2_cR4_d2500_r7.5
             echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
             echo "cF${cFarray[i]}_cRF${cRFarray[j]}"
             echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-        mpirun -np 2 lmp_auto < in.r_yM150e6_ps2_cR2_d2500_r7.5
+        mpirun -np 2 lmp_auto < in.r_yM150e6_ps2_cR4_d2500_r7.5
     done
 done
 
